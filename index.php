b5f977df7a1c20698c81a49dc221c0c2f16ea48a
UnrealTemplier <jaimore.evseev@gmail.com>
2025-02-28 20:56:35 +0400

    Extract database interaction into Database class.

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 <?php
 
 require "functions.php";
+require "Database.php";
 //require "router.php";
 
-$dsn = "mysql:host=localhost;port=3306;dbname=demo;charset=utf8mb4";
-$pdo = new PDO($dsn, "root");
-
-$statement = $pdo->prepare("SELECT * FROM users");
-$statement->execute();
-
-$users = $statement->fetchAll(PDO::FETCH_ASSOC);
+$database = new Database();
+$users = $database->query("SELECT * FROM users")->fetchAll(PDO::FETCH_ASSOC);
 
 echo "<h3>Users:</h3>";
 echo "<ul>";
@@ -18,10 +14,8 @@ foreach ($users as $user) {
 }
 echo "</ul>";
 
-$statement = $pdo->prepare("SELECT * FROM posts");
-$statement->execute();
 
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+$posts = $database->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
 
 echo "<h3>Posts:</h3>";
 echo "<ul>";
