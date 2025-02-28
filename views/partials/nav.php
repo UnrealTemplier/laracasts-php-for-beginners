ce6cb8e36582430627e73796f85476339d292327
UnrealTemplier <jaimore.evseev@gmail.com>
2025-02-28 18:36:43 +0400

    Add conditional styling using $_SERVER superglobal to nav.php partial.

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,3 +1,13 @@
+<?php
+
+$home = "/";
+$about = "/about.php";
+$contact = "/contact.php";
+
+$nav = "text-gray-300 hover:bg-gray-700 hover:text-white";
+$navSelected = "bg-gray-900 text-white";
+?>
+
 <nav class="bg-gray-800">
   <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
     <div class="flex h-16 items-center justify-between">
@@ -8,9 +18,9 @@
         <div class="hidden md:block">
           <div class="ml-10 flex items-baseline space-x-4">
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-            <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
-            <a href="/about.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-            <a href="/contact.php" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+            <a href="<?= $home ?>" class="<?= urlIs($home) ? $navSelected : $nav ?> rounded-md  px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
+            <a href="<?= $about ?>" class="<?= urlIs($about) ? $navSelected : $nav ?> rounded-md px-3 py-2 text-sm font-medium">About</a>
+            <a href="<?= $contact ?>" class="<?= urlIs($contact) ? $navSelected : $nav ?> rounded-md px-3 py-2 text-sm font-medium">Contact</a>
           </div>
         </div>
       </div>
