1f7929e3927fa55ba7ee46c4b2b36960bb879b6d
UnrealTemplier <jaimore.evseev@gmail.com>
2025-03-01 12:18:49 +0400

    Bind params to SQL query.

--- index.php
+++ index.php
@@ -9,19 +9,8 @@ $config = require("config.php");
 $database = new Database($config["database"]);
 $users = $database->query("SELECT * FROM users")->fetchAll();
 
-echo "<h3>Users:</h3>";
-echo "<ul>";
-foreach ($users as $user) {
-    echo "<li>{$user["name"]}</li>";
-}
-echo "</ul>";
+$id = $_GET["id"];
+$query = "SELECT * FROM posts WHERE id=:id";
 
-
-$posts = $database->query("SELECT * FROM posts")->fetchAll();
-
-echo "<h3>Posts:</h3>";
-echo "<ul>";
-foreach ($posts as $post) {
-    echo "<li>{$post["title"]}</li>";
-}
-echo "</ul>";
+$posts = $database->query($query, ["id" => $id])->fetchAll();
+dd($posts);
