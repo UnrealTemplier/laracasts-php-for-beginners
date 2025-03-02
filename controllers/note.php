b5e7993635550a2b5b06b5818eadf366e8ee6e34
UnrealTemplier <jaimore.evseev@gmail.com>
2025-03-02 16:12:15 +0400

    Restrict access to notes created by other users (first step to authentication and authorization);

--- controllers/note.php
+++ controllers/note.php
@@ -1,10 +1,21 @@
 <?php
 
 $heading = "Note";
+$currentUserId = 1;
 
 $config = require("config.php");
 $db = new Database($config["database"]);
 
-$note = $db->query("SELECT * FROM notes WHERE id = :id", ["id" => $_GET["id"]])->fetch();
+$note = $db->query("SELECT * FROM notes WHERE id = :id", [
+    "id" => $_GET["id"],
+])->fetch();
+
+if (!$note) {
+    abort();
+}
+
+if ($note["user_id"] !== $currentUserId) {
+    abort(Response::FORBIDDEN);
+}
 
 require "views/note.view.php";
