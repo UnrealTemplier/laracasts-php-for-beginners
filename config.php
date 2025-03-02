b8d27b9d61cc29788618c4a2169387fc0f6be701
UnrealTemplier <jaimore.evseev@gmail.com>
2025-03-02 12:58:40 +0400

    Change dbname in config for Notes mini-project.

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ return [
     "database" => [
         "host" => "localhost",
         "port" => "3306",
-        "dbname" => "demo",
+        "dbname" => "notes-mini-project",
         "charset" => "utf8mb4",
     ],
 ];
