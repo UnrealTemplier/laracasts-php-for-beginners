1a3ef7b4ef8eff0e2edc65522685cd162ed0e3e6
UnrealTemplier <jaimore.evseev@gmail.com>
2025-03-04 17:31:03 +0400

    Add first validation attempts.

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -6,10 +6,22 @@ $config = require 'config.php';
 $db = new Database($config['database']);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
-        ':body' => $_POST['body'],
-        ':user_id' => 1,
-    ]);
+    $errors = [];
+
+    if (strlen($_POST['body']) === 0) {
+        $errors['body'] = 'This field is required.';
+    }
+
+    if (strlen($_POST['body']) > 1000) {
+        $errors['body'] = 'This field cannot be longer than 1,000 characters.';
+    }
+
+    if (empty($errors)) {
+        $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+            ':body' => $_POST['body'],
+            ':user_id' => 1,
+        ]);
+    }
 }
 
 require 'views/note-create.view.php';
--- views/note-create.view.php
+++ views/note-create.view.php
@@ -7,15 +7,28 @@
 
       <form method="POST">
         <div class="space-y-12">
-          <div class="border-b border-gray-900/10">
+          <div>
 
             <div class="col-span-full">
               <label for="body" class="block text-sm/6 font-medium text-gray-900">Text of your note</label>
-              <div class="mt-2">
-                <textarea name="body" id="body" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" placeholder="Your note goes here..."></textarea>
-              </div>
-            </div>
 
+              <div class="mt-2">
+                <textarea
+                  name="body"
+                  id="body"
+                  rows="3"
+                  class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  placeholder="Your note goes here..."
+                ><?= $_POST['body'] ?? '' ?></textarea>
+              </div>
+
+                <?php if (isset($errors['body'])): ?>
+                  <p class="text-red-500 text-xs mt-1 ml-2">
+                      <?= $errors['body'] ?>
+                  </p>
+                <?php endif; ?>
+
+            </div>
           </div>
         </div>
 
