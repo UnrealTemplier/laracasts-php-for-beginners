04f8c2c4904c4607113877d95a0ce59e90726f00
UnrealTemplier <jaimore.evseev@gmail.com>
2025-03-05 11:52:38 +0400

    Add currentUserId variable (temporary authentication) to /notes/index controller.

--- controllers/notes/index.php
+++ controllers/notes/index.php
@@ -2,10 +2,14 @@
 
 use Core\Database;
 
+$currentUserId = 1;
+
 $config = require basePath('config.php');
 $db = new Database($config['database']);
 
-$notes = $db->query('SELECT * FROM notes WHERE user_id = 1')->get();
+$notes = $db->query('SELECT * FROM notes WHERE user_id = :user_id', [
+    'user_id' => $currentUserId,
+])->get();
 
 view('notes/index.view.php', [
     'heading' => 'My Notes',
